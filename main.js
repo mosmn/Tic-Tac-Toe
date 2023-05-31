c5ee02d95c1f91b34daf657719b7f24b1c6bc0c2
mosmn <ithinkisee8@gmail.com>
2023-05-31 14:16:57 +0800

    g

--- main.js
+++ main.js
@@ -9,27 +9,125 @@ const gameBoard = (() => {
         board[index] = sign;
     }
 
+    const getBoard = () => board;
+
     const resetBoard = () => {
         board = new Array(9);
     }
 
-    return {getCell, setCell, resetBoard};
+    return {
+        getCell,
+        setCell,
+        getBoard,
+    };
 })();
 
 const Player = (sign) => {
     this.sign = sign;
     const getSign = () => sign;
 
-    const changeSign = () => {};
-    
-    return {getSign};
+    // const changeSign = () => {};
+
+    return {
+        getSign
+    };
 }
 
 // const minmaxAiLogic = (() => {
 // })();
 
 const gameController = (() => {
+    const player1 = Player('X');
+    const player2 = Player('O');
+    let currentPlayer = player1;
+
+    const switchPlayer = () => {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        }
+        else {
+            currentPlayer = player1;
+        }
+    }
+
+    const checkWin = (board) => {
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+            [0, 4, 8], [2, 4, 6] // diagonal
+        ];
+
+        for (let i = 0; i < winningCombos.length; i++) {
+            const [a, b, c] = winningCombos[i];
+            if (board[a] === board[b] && board[b] === board[c]) {
+                return board[a];
+            }
+        }
+        return null;
+    }
+
+    const checkTie = (board) => {
+        return board.every(cell => cell !== undefined);
+    }
+
+    const play = (cell) => {
+        const board = gameBoard.getBoard();
+        if (board[cell] === undefined) {
+            gameBoard.setCell(cell, currentPlayer.getSign());
+            const winner = checkWin(board);
+            if (winner) {
+                displayController.displayMessage(`${winner} wins!`);
+                return;
+            }
+            if (checkTie(board)) {
+                displayController.displayMessage('Tie!');
+                return;
+            }
+            switchPlayer();
+        }
+    }
+
+    return {
+        play,
+    };
 })();
 
 const displayController = (() => {
+    const cells = document.querySelectorAll('.cell');
+    const message = document.querySelector('.message');
+    const resetBtn = document.querySelector('#restart');
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', (e) => {
+            const cell = e.target.id;
+            gameController.play(cell);
+        });
+    });
+
+    const displayMessage = (msg) => {
+        message.textContent = msg;
+    }
+
+    const resetMessage = () => {
+        message.textContent = '';
+    }
+
+    const resetBoard = () => {
+        cells.forEach(cell => {
+            cell.textContent = '';
+        });
+    }
+
+    const reset = () => {
+        resetMessage();
+        resetBoard();
+    }
+
+    resetBtn.addEventListener('click', reset);
+
+    return {
+        displayMessage,
+        resetMessage,
+        resetBoard,
+    };
 })();
